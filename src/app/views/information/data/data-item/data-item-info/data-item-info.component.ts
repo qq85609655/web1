49debe6e4c1c5f60bbc971a58cdfd848c266c913
Administrator <253479240@QQ.COM>
2018-04-26 19:54:38 +0800

    和工业化

--- src/app/views/information/data/data-item/data-item-info/data-item-info.component.ts
+++ src/app/views/information/data/data-item/data-item-info/data-item-info.component.ts
@@ -9,6 +9,40 @@ export class DataItemInfoComponent extends BaseComponent implements OnInit {
     //this.initSourceIdAndType(1 );
   }
 
+
+  queryParam = {
+    nodeId: 0
+  };
+
+  public tableEvent: EventEmitter<any> = new EventEmitter();
+
+  public codeTableOpts = {
+    that: this,
+    queryMethod: 'post',
+    queryUrl: 'codestandard/queryCodeALL',
+    pageParam: {
+      pageNum: 1,
+      pageSize: 1000
+    },//可使用默认值
+    isPage: false,//是否分页
+    defaultPageSize: 1000,
+    queryParam: this.queryParam,//页面选择的查询参数，包括树节点id等信息
+    bodyParam: this.queryParam,//请求体中的参数
+    queryResultField: ['codeId'],//第一个值指定id的字段名,主要用于修改删除，状态切换
+    tableType: 'single',//树类型，simple/checkbox
+    theadOptions: [
+      {name: '编号', type: 'numberpage'},
+      {name: '代码', field: 'code'},
+      {name: '中文名称', field: 'name'},
+      {name: '描述', field: 'description'},
+    ],
+    usingCache: false,
+    buttonOptions: [],
+    selections: [],
+    emptyMessage: '暂无数据',
+    tableEvent: this.tableEvent
+  };
+
   initSourceIdAndType(sourceId: number) {
     this.sourceId = sourceId;
     let type = '';
@@ -104,105 +138,122 @@ export class DataItemInfoComponent extends BaseComponent implements OnInit {
       label: '国家教育代码'
     }];
 
-  public DataReferencedList2 = [{value: '', label: '请选择'}];
+  public DataReferencedList2 = [];
 
-  public DataReferencedList3 = [{value: '', label: '请选择'}];
+  public DataReferencedList3 = [];
 
 
-  public isShow = false;
+  public isShow1 = true;
+  public isShow2 = false;
+  public isShow3 = false;
+  public isShow4 = false;
 
   public change1() {
     var dt = this.itemData;
     var ref1 = dt.dataReferenced1;
+    console.info(ref1);
     //接着 加载2级类
     this.getHttpClient().get(
-      'datastandard/querySecondNodes',
-      {parentNode: ref1},
+      'codestandard/queryNodesByParentId',
+      {parentNode: ref1, type: 1},
       data => {
         this.DataReferencedList2 = [{value: '', label: '请选择'}];
         if (data && data.length > 0) {
           for (let d of data) {
-            this.DataReferencedList2.push({value: d.code, label: d.name});
+            console.info(d.nodeId + '----' + d.name);
+            this.DataReferencedList2.push({value: d.nodeId, label: d.name});
           }
-          this.isShow = true;
+          this.isShow2 = true;
+          if (ref1 == '1000') {
+            this.isShow3 = true;
+          } else {
+            this.isShow3 = false;
+          }
+          this.isShow4 = false;
         }
       }
     );
   }
 
   changeForReferenced() {
+    // debugger;
+    this.isShow1 = false;
     var dtt = this.itemData;
     var keyflag = dtt.dataPrimarykey;
     if (keyflag == 1) {
-      this.isShow = false;
+      this.isShow1 = false;
+      this.isShow2 = false;
+      this.isShow3 = false;
+      this.isShow4 = false;
+    } else {
+      this.isShow1 = true;
     }
   }
 
   public change2() {
+    // debugger;
     var dt = this.itemData;
-    var ref2 = dt.dataReferenced2;
+    let ref2 = dt.dataReferenced2;
+    // console.info(ref2);
     //接着 加载2级类
     this.getHttpClient().get(
-      'datastandard/getThreeNodes',
-      {parentNode: ref2},
+      'codestandard/queryNodesByParentId',
+      {parentNode: ref2, type: 2},
       data => {
-        this.DataReferencedList3 = [{value: '', label: '请选择'}];
+        console.info(data.length);
         if (data && data.length > 0) {
           for (let d of data) {
-            this.DataReferencedList3.push({value: d.code, label: d.name});
+            console.info(d.nodeId + '----' + d.name);
+            this.DataReferencedList3.push({value: d.nodeId, label: d.name});
           }
-          this.isShow = true;
+          this.isShow3 = true;
+        } else {//说明 此时到了最后一级  那就显示 其代码值 列表 供参考
+          this.isShow3 = false;
+          this.isShow4 = true;
+          this.flushTable(ref2);
         }
       }
     );
   }
 
+  public flushTable(v: any) {
+    console.info('ref========', v);
+    this.queryParam.nodeId = parseInt(v);
+    this.tableEvent.emit({flush: true});
+  }
 
   public change3() {
     var dt = this.itemData;
     var ref3 = dt.dataReferenced3;
     //接着 加载2级类
     this.getHttpClient().get(
-      'datastandarditem/getThreeNodes',
-      {nodeId: ref3},
+      'codestandard/queryNodesByParentId',
+      {parentNode: ref3, type: 3},
       data => {
         if (data != null) {
-          this.isShow = true;
+          this.isShow4 = true;
           //展示详情：
-
+          this.queryParam.nodeId = parseInt(ref3);
+          this.tableEvent.emit({flush: true});
         }
       }
     );
   }
 
-  public codeTableOpts = {
-    that: this,
-    queryMethod: 'post',
-    queryUrl: 'codestandard/queryCodeList2',
-    pageParam: {
-      pageNum: 1,
-      pageSize: 10
-    },//可使用默认值
-    isPage: true,//是否分页
-    defaultPageSize: 10,
-    queryParam: '',//页面选择的查询参数，包括树节点id等信息
-    bodyParam: '',//请求体中的参数
-    queryResultField: ['codeId'],//第一个值指定id的字段名,主要用于修改删除，状态切换
-    tableType: 'single',//树类型，simple/checkbox
-    theadOptions: [
-      {name: '编号', type: 'numberpage'},
-      {name: '代码', field: 'code'},
-      {name: '中文名称', field: 'name'},
-      {name: '描述', field: 'description'},
-    ],
-    buttonOptions: [],
-    selections: [],
-    emptyMessage: '暂无数据',
-    tableEvent: null
-  };
+  public show1() {
+    return this.isShow1;
+  }
 
-  public show() {
-    return this.isShow;
+  public show2() {
+    return this.isShow2;
+  }
+
+  public show3() {
+    return this.isShow3;
+  }
+
+  public show4() {
+    return this.isShow4;
   }
 
 
