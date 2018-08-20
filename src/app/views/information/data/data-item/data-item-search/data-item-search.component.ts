0668a75fa96a16c4c13308a3c08edb1382f4ed61
Administrator <253479240@QQ.COM>
2018-08-20 14:02:23 +0800

    UIuuy

--- src/app/views/information/data/data-item/data-item-search/data-item-search.component.ts
+++ src/app/views/information/data/data-item/data-item-search/data-item-search.component.ts
@@ -1,8 +1,8 @@
 import {AfterViewInit, Component, EventEmitter, OnInit} from '@angular/core';
-import {BaseComponent} from "../../../../../components/base/base.component";
-import {ActivatedRoute} from "@angular/router";
+import {BaseComponent} from '../../../../../components/base/base.component';
+import {ActivatedRoute} from '@angular/router';
 
-export class DataItemSearchComponent extends BaseComponent implements OnInit,AfterViewInit {
+export class DataItemSearchComponent extends BaseComponent implements OnInit, AfterViewInit {
 
   constructor(public _ActivatedRoute: ActivatedRoute) {
     super();
@@ -10,7 +10,7 @@ export class DataItemSearchComponent extends BaseComponent implements OnInit,Aft
   }
 
   public sourceId = 1;
-  public treeNode:any;
+  public treeNode: any;
   public treeQueryParam = {
     sourceId: 1
   };
@@ -18,18 +18,18 @@ export class DataItemSearchComponent extends BaseComponent implements OnInit,Aft
     that: this,
     queryMethod: 'get',
     expandedIndex: 0,
-    queryUrl: "datastandard/queryDataOrgTree",
+    queryUrl: 'datastandard/queryDataOrgTree',
     queryParam: this.treeQueryParam,//表示query类型参数，放在?后面。后续如果需要pathParam bodyParam再调整
     functionName: '元数据',//新增和修改框标题中的功能名称
     queryResultField: ['code', 'parentCode', 'name', 'children'],//查询结果对象中，需要的字段名称
     treeType: 'single',//树类型，simple/checkbox
     nodeSelect: this.treeNodeSelect,
     operButton: {},
-    usingCache:false,
+    usingCache: false,
     treeEvent: new EventEmitter()
   };
 
-  public tableEvent:EventEmitter<any> = new EventEmitter();
+  public tableEvent: EventEmitter<any> = new EventEmitter();
   public queryParam = {
     sourceId: 1,
     keyWord: '',
@@ -38,12 +38,12 @@ export class DataItemSearchComponent extends BaseComponent implements OnInit,Aft
   };
   public queryParam2 = {
     sourceId: 1
-  }
+  };
 
-  public tableOpts= {
+  public tableOpts = {
     that: this,
     queryMethod: 'post',
-    queryUrl: "datastandarditem/queryItemList",
+    queryUrl: 'datastandarditem/queryItemList',
     pageParam: {
       pageNum: 1,
       pageSize: 10
@@ -51,7 +51,7 @@ export class DataItemSearchComponent extends BaseComponent implements OnInit,Aft
     isPage: true,//是否分页
     defaultPageSize: 10,
     queryParam: this.queryParam2,//页面选择的查询参数，包括树节点id等信息
-    bodyParam:this.queryParam,//请求体中的参数
+    bodyParam: this.queryParam,//请求体中的参数
     queryResultField: ['code'],//第一个值指定id的字段名,主要用于修改删除，状态切换
     tableType: 'single',//树类型，simple/checkbox
     usingCache: false,
@@ -62,55 +62,66 @@ export class DataItemSearchComponent extends BaseComponent implements OnInit,Aft
     //普通的字段可支持link、format， format后续需要再增加实现
     //button表示按钮列，每一列可以多个按钮，根据options指定字段可以给多列都使用按钮
     theadOptions: [
-      {name:'编号', field: 'itemCode'},
-      {name:'数据项名', field: 'itemName', link: this.linkDataDetail},
-      {name:'中文名称', field: 'itemComment'},
-      {name:'所属子类', field: 'subclassName'},
-      {name:'说明/示例', field: 'dataExplain'},
-      {name:'操作', type:'button', buttonOptions: 'buttonOptions'}
+      {name: '编号', field: 'itemCode'},
+      {name: '数据项名', field: 'itemName', link: this.linkDataDetail},
+      {name: '中文名称', field: 'itemComment'},
+      {name: '所属子类', field: 'subclassName'},
+      {name: '说明/示例', field: 'dataExplain'},
+      {name: '操作', type: 'button', buttonOptions: 'buttonOptions'}
     ],
     //callback指定回调的方法，
     //disabled指定按钮是否禁用
     //hidden指定按钮是否隐藏
     //此三个方法参数一样,共俩个，第一个index,第二个当前行item
-    buttonOptions:[
-      {name:'修改', callback: this.updateData, disabled: null, hidden: null, authcode:'027003'},
-      {name:'删除', callback: this.deleteData, disabled: null, hidden: null, authcode:'027004'},
+    buttonOptions: [
+      {name: '修改', callback: this.updateData, disabled: null, hidden: null, authcode: '027003'},
+      {name: '删除', callback: this.deleteData, disabled: null, hidden: null, authcode: '027004'},
     ],
-    selections:[],
-    emptyMessage:'暂无数据',
+    selections: [],
+    emptyMessage: '暂无数据',
     tableEvent: this.tableEvent
   };
 
-  dialogOpts={
-    delete:{
+  dialogOpts = {
+    delete: {
       title: '元数据删除',
-      visible:false,
+      visible: false,
       data: {}
-    }
-  }
-
-  ngOnInit() {
-    this._ActivatedRoute.queryParams.subscribe(params=>{
-      if(params.back == "true"){
-        this.treeOpts.usingCache = true;
-        this.tableOpts.usingCache = true;
+    },
+    runSql2: {
+      title: '立即执行',
+      visible: false,
+      param: {
+        code: '',
+        nodeType: 0
       }
-    });
+    },
+    runSql: {
+      title: '查询结果展示',
+      visible: false,
+      data: {
+        itemDetailVos: [],
+        datas: [],
+        sqlName: '',
+        dataCount: 0
+      }
+    }
+  };
+
+  seeData() {
+    if (this.treeNode.data.nodeType != 3) {
+      this.tipWarnMessage('请选择数据子类节点后进行查询！');
+      return;
+    }
+    this.dialogOpts.runSql2.visible = true;
+    this.dialogOpts.runSql2.param.code = this.queryParam.code;
   }
 
-  ngAfterViewInit(){}
-
-  flushData() {
-    this.tableEvent.emit({flush:true});
+  runOk() {
+    this.dialogOpts.runSql2.visible = false;
+    this.runItem(this.dialogOpts.runSql2.param.code);
   }
 
-  initSourceId(sourceId:number){
-    this.sourceId = sourceId;
-    this.queryParam.sourceId = sourceId;
-    this.queryParam2.sourceId = sourceId;
-    this.treeQueryParam.sourceId = sourceId;
-  }
 
   /**
    * 树节点点击事件
@@ -123,74 +134,110 @@ export class DataItemSearchComponent extends BaseComponent implements OnInit,Aft
     this.flushData();
   }
 
-  linkDataDetail(index ,item){
-    var queryParams = {
-      itemCode : item.itemCode,
-      id: item.id
-    };
-    this.saveSelectToLink(this.treeOpts.treeEvent , this.tableOpts.tableEvent , ()=>{
-      if(this.sourceId == 1) {
-        this.getRouter().navigate(["index/CountryMeta/detail"], {queryParams: queryParams});
-      }else{
-        this.getRouter().navigate(["index/school/SchoolMeta/detail"], {queryParams: queryParams});
+  runItem(code) {
+    this.getHttpClient().post('datastandarditem/queryDataShow/' + code, null, null, (data) => {
+      this.dialogOpts.runSql.visible = true;
+      this.dialogOpts.runSql.data.datas = data.datas;//数据结果
+      this.dialogOpts.runSql.data.itemDetailVos = data.itemDetailVos; // 表头
+      this.dialogOpts.runSql.data.sqlName = data.sqlName; //对应表名
+      this.dialogOpts.runSql.data.dataCount = data.dataCount; //表现在的数据总量
+    });
+  }
+
+  ngOnInit() {
+    this._ActivatedRoute.queryParams.subscribe(params => {
+      if (params.back == 'true') {
+        this.treeOpts.usingCache = true;
+        this.tableOpts.usingCache = true;
       }
     });
   }
 
-  addData(){
-    if(this.treeNode.data.nodeType != 3){
-      this.tipWarnMessage("请选择数据子类节点后新增！");
+  ngAfterViewInit() {
+  }
+
+  flushData() {
+    this.tableEvent.emit({flush: true});
+  }
+
+  initSourceId(sourceId: number) {
+    this.sourceId = sourceId;
+    this.queryParam.sourceId = sourceId;
+    this.queryParam2.sourceId = sourceId;
+    this.treeQueryParam.sourceId = sourceId;
+  }
+
+
+  linkDataDetail(index, item) {
+    var queryParams = {
+      itemCode: item.itemCode,
+      id: item.id
+    };
+    this.saveSelectToLink(this.treeOpts.treeEvent, this.tableOpts.tableEvent, () => {
+      if (this.sourceId == 1) {
+        this.getRouter().navigate(['index/CountryMeta/detail'], {queryParams: queryParams});
+      } else {
+        this.getRouter().navigate(['index/school/SchoolMeta/detail'], {queryParams: queryParams});
+      }
+    });
+  }
+
+  addData() {
+    if (this.treeNode.data.nodeType != 3) {
+      this.tipWarnMessage('请选择数据子类节点后新增！');
       return;
     }
     var queryParams = {
       subclassCode: this.treeNode.data.code,
       subclassName: this.treeNode.data.name
     };
-    this.saveSelectToLink(this.treeOpts.treeEvent , this.tableOpts.tableEvent , ()=>{
-      if(this.sourceId == 1){
-        this.getRouter().navigate(["index/CountryMeta/add"], {queryParams: queryParams})
-      }else{
-        this.getRouter().navigate(["index/school/SchoolMeta/add"], {queryParams: queryParams})
+    this.saveSelectToLink(this.treeOpts.treeEvent, this.tableOpts.tableEvent, () => {
+      if (this.sourceId == 1) {
+        this.getRouter().navigate(['index/CountryMeta/add'], {queryParams: queryParams});
+      } else {
+        this.getRouter().navigate(['index/school/SchoolMeta/add'], {queryParams: queryParams});
       }
     });
 
   }
 
-  updateData(index,item){
+  updateData(index, item) {
     var queryParams = {
       itemCode: item.itemCode,
       id: item.id
     };
-    this.saveSelectToLink(this.treeOpts.treeEvent , this.tableOpts.tableEvent , ()=>{
-      if(this.sourceId == 1){
-        this.getRouter().navigate(["index/CountryMeta/edit"], {queryParams: queryParams})
-      }else{
-        this.getRouter().navigate(["index/school/SchoolMeta/edit"], {queryParams: queryParams})
+    this.saveSelectToLink(this.treeOpts.treeEvent, this.tableOpts.tableEvent, () => {
+      if (this.sourceId == 1) {
+        this.getRouter().navigate(['index/CountryMeta/edit'], {queryParams: queryParams});
+      } else {
+        this.getRouter().navigate(['index/school/SchoolMeta/edit'], {queryParams: queryParams});
       }
     });
   }
 
-  deleteData(index,item){
+  deleteData(index, item) {
     this.dialogOpts.delete = {
       title: '元数据删除',
-      visible:true,
-      data: {id: item.id, sourceId : this.sourceId}
+      visible: true,
+      data: {id: item.id, sourceId: this.sourceId}
     };
   }
 
-  deleteDataOk(){
-    var url = "datastandarditem/deleteItemVo";
-    this.getHttpClient().get( url, this.dialogOpts.delete.data, (data)=>{
-      this.tipMessage("删除" + this.getFunctionName()+"成功!");
+  deleteDataOk() {
+    var url = 'datastandarditem/deleteItemVo';
+    this.getHttpClient().get(url, this.dialogOpts.delete.data, (data) => {
+      this.tipMessage('删除' + this.getFunctionName() + '成功!');
       this.flushData();
       this.dialogOpts.delete.visible = false;
     });
   }
 
-  getFunctionName(){
-    return this.sourceId == 1 ? '元数据':'元数据';
+  getFunctionName() {
+    return this.sourceId == 1 ? '元数据' : '元数据';
   }
-  getFunctionParentName(){
-    return this.sourceId == 1 ? '国家数据标准':'学校数据标准';
+
+  getFunctionParentName() {
+    return this.sourceId == 1 ? '国家数据标准' : '学校数据标准';
   }
+
 }
